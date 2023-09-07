7de97115e67526b02cc0a16360675a11182b294b
mg5714 <mistergddora2000@gmail.com>
2023-09-07 17:20:35 +0200

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
- *     
+ * main - C program that prints exactly "Programming is like
+ *                building a multilingual puzzle, followed by a new line.
  *
- * 
-*/
+ * return: Always 0.
+ */
 
 int main(void)
 {
