b46cb3d871725b2c5e13d98fe72add46cf841f5c
mg5714 <mistergddora2000@gmail.com>
2023-09-07 21:53:11 +0200

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - C program that prints exactly "Programming is like
+ *                building a multilingual puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+putchar(str);
+return(1);
+}
