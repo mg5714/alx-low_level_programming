aa47884b578e12337bf42dd725119b6020b4ee9a
mg5714 <mistergddora2000@gmail.com>
2023-09-07 17:39:35 +0200

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - C program that prints exactly "Programming is like
- *                building a multilingual puzzle, followed by a new line.
+ * main - C program that prints exactly with proper grammar, but the outcome
+ *                is a piece of art,, followed by a new line.
  *
  * Return: Always 0.
  */
