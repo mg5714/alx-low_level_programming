265f07390418125483ce872646e368c7a03c11fa
mg5714 <mistergddora2000@gmail.com>
2023-09-07 18:42:57 +0200

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**                                                                                                                                 
+* main - C program that prints the size of various types on the computer                                                                                        
+* Return: Always 0 (Success)                                                                                                       
+*/
+
+int main(void)
+{
+
+printf("Size of a char:%zu", byte(s));
+
+printf("Size of an int:%zu", byte(s));
+
+printf("Size of a long int:%zu", byte(s));
+
+printf("Size of a long long int:%zu", byte(s));
+
+printf("Size of a float:%zu", byte(s));
+
+return=0;
+ 
+}
