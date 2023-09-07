f3daaf180e82ca25870042e92c272d48b677a454
mg5714 <mistergddora2000@gmail.com>
2023-09-07 17:05:58 +0200

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-
-int main (void){
+int main(void){
   char text[] = "\"Programming is like building a multilingual puzzle";
-    puts(text);
+  puts (text);
 
-    return (0);
+  return (0);
 
 }
