9d343bf69e504941847811278cf4b1fac8dcc5a4
mg5714 <mistergddora2000@gmail.com>
2023-09-07 17:38:00 +0200

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -10,7 +10,7 @@
 int main(void)
 {
 
-char text[] = "with proper grammar, but the outcome is a piece of art,";
+char text[] = "with proper grammar, but the outcome is a piece of art,\n";
 
 printf("%s", text);
 
