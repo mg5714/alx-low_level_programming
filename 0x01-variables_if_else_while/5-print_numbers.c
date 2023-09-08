9d58a2c30973a3ac19a214a2d6c3f82123b17bce
mg5714 <mistergddora2000@gmail.com>
2023-09-08 17:08:12 +0200

    task 4

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: 'C program that prints all single digit numbers of base 10'
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+for (i=0; i < 10 ; i++)  
+{
+printf("%d", i);
+}
+return (0);
+}
