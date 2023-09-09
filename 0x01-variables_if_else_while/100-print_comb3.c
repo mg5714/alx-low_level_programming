b06a7036a28037db5257067a98eb5256aa95d7e3
mg5714 <mistergddora2000@gmail.com>
2023-09-09 16:31:00 +0200

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
 * main - program that prints all possible different
-*             combinations of two digits.          
+*             combinations of two digits.
 * Return: Always 0.
 */
 int main(void)
