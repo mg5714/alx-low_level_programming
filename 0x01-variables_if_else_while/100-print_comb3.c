03e1992d47811aab00cd8bbd802f4bfc0e020750
mg5714 <mistergddora2000@gmail.com>
2023-09-09 16:05:17 +0200

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,8 +10,8 @@ for (i = 0; i < 10; i++)
 {
 for (x = 0; x < 10; x++)
 {
-putchar(i + '0');
-putchar(x + '0');
+putchar((i % 10) + '0');
+putchar((x % 10) + '0');
 if (i == 9 && x == 9)
 continue;
 putchar(',');
