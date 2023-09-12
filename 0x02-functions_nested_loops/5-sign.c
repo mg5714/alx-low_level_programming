cb2e303ec70d60a3369556e080d482ea80a5e311
mg5714 <mistergddora2000@gmail.com>
2023-09-12 22:35:33 +0200

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,15 +6,21 @@
  * 0 and prints 0 if n is zero
  * -1 and prints - if n is less than zero
  */
-int print_sign(int n) {
-if (n > 0) {
+int print_sign(int n)
+{
+if (n > 0)
+{
 _putchar('+');
-return 1;
-} else if (n == 0) {
+return (1);
+}
+else if (n == 0)
+{
 _putchar('0');
-return 0;
-} else {
+ return (0);
+}
+else
+{
 _putchar('-');
-return -1;
+ return (-1);
 }
 }
