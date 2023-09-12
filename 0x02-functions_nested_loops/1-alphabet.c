cb554e14ff90231c1ebb06834b9084ba28cb1dda
mg5714 <mistergddora2000@gmail.com>
2023-09-12 17:23:05 +0200

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: 'C program that prints the alphabet in lowercase,and upper'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alphabet;
+for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
+{
+putchar(alphabet);
+}
+putchar('\n');
+return (0);
+}
