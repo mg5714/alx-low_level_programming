210cc418d329b37352c4da04188c5f9608a114d1
mg5714 <mistergddora2000@gmail.com>
2023-09-12 18:05:41 +0200

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+char alphabet;
+for (i = 0; i < 10; i++)
+{
+for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
+{
+_putchar(alphabet);
+}
+}
+_putchar('\n');
+}
