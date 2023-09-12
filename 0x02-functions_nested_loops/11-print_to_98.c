bb6c2e828c5c145d1907d7a1d4211f9ef704edbd
mg5714 <mistergddora2000@gmail.com>
2023-09-12 23:31:51 +0200

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,25 +6,25 @@
  */
 void print_to_98(int n)
 {
-  int i, j;
-if (n <= 98) {
-for (int i = n; i <= 98; i++) {
-_putchar(i / 10 + '0'); // print tens digit
-_putchar(i % 10 + '0'); // print ones digit
-if (i != 98) {
-_putchar(','); // print comma
-_putchar(' '); // print space
-}
-}
-} else {
-for (int i = n; i >= 98; i--) {
-_putchar(i / 10 + '0'); // print tens digit
-_putchar(i % 10 + '0'); // print ones digit
-if (i != 98) {
-_putchar(','); // print comma
-_putchar(' '); // print space
-}
-}
-}
-_putchar('\n'); // print newline character
+	int i, j;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+				printf("%d, ", i);
+			else if (i == 98)
+				printf("%d\n", i);
+		}
+	} else if (n >= 98)
+	{
+		for (j = n; j >= 98; j--)
+		{
+			if (j != 98)
+				printf("%d, ", j);
+			else if (j == 98)
+				printf("%d\n", j);
+		}
+	}
 }
