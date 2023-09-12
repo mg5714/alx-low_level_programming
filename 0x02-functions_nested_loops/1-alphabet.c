d1f2e3083205499d3c2788496bb9615c2edf899a
mg5714 <mistergddora2000@gmail.com>
2023-09-12 17:48:28 +0200

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,7 +4,7 @@
  * Description: 'C program that prints the alphabet in lowercase,and upper'
  * Return: Always 0 (Success)
  */
-int main(void)
+void print_alphabet(void)
 {
 char alphabet;
 for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
