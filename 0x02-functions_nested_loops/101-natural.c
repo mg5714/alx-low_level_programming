d2cc7504d02e28ed696f684f4a78d99f7148bd39
mg5714 <mistergddora2000@gmail.com>
2023-09-12 23:52:41 +0200

    task 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,7 +7,7 @@
 int main(void)
 {
 int sum = 0;
- int i;
+int i;
 for (i = 0; i < 1024; i++)
 {
 if (i % 3 == 0 || i % 5 == 0)
@@ -16,5 +16,5 @@ sum += i;
 }
 }
 printf("%d\n", sum);
- return (0);
+return (0);
 }
