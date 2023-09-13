893f40bd7390ad01bf72aff9393e318c95bbb809
mg5714 <mistergddora2000@gmail.com>
2023-09-13 15:23:19 +0200

    done 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,27 +1,16 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include "main.h"
+
 /**
-* main - Entry point
-* Description: 'Positive anything is better than negative nothing '
-* Return: Always 0 (Success)
+* main - tests function that prints if integer is positive or negative
+* Return: 0
 */
+
 int main(void)
 {
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-if (n > 0)
-{
-printf("%d is positive\n", n);
-}
-else if (n < 0)
-{
-printf("%d is negative\n", n);
-}
-else if (n == 0)
-{
-printf("%d is zero\n", n);
-}
-return (0);
+        int i;
+
+        i = 98;
+        positive_or_negative(i);
+
+        return (0);
 }
