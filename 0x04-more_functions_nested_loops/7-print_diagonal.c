8e8326075c00f7a5c7f1db074aa9bfa346c804d8
mg5714 <mistergddora2000@gmail.com>
2023-09-14 20:19:46 +0200

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*print_line - prints a straight line
+*@n:parameter
+*Return:returns nothing
+*/
+void print_line(int n)
+{
+for (i = 0; i < n; i++)
+{
+putchar('\\');
+}
+putchar('\n');
+}
