7616cc0c57b6a9ab970f4ffd6f423213051aabdd
mg5714 <mistergddora2000@gmail.com>
2023-09-14 18:41:46 +0200

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,10 +15,10 @@ for (number2 = 0; number2 <= 14; number2++)
 {
 if (number2 > 9)
 {
-_putchar((number2 / 10) + '0');
+putchar((number2 / 10) + '0');
 }
-_putchar((number2 % 10) + '0');
+putchar((number2 % 10) + '0');
 }
-_putchar(10);
+putchar(10);
 }
 }
