8013e3aff445c9767c3c76a4566d6fada9f51349
mg5714 <mistergddora2000@gmail.com>
2023-09-14 20:49:11 +0200

    task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*print_square - prints squares
+*@size: parameter
+*Return:returns nothing
+*/
+void print_square(int size)
+{
+int i, j, Side;
+for(i = 0; i < Side; i++)
+{
+for(j = 0; j < Side; j++)
+{
+putchar('*');
+}
+putchar('\n');
+}
+return 0;
+}
