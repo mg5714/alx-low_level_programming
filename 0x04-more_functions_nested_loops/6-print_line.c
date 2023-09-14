f61698813338261d7029e445d9a0325af58d1e47
mg5714 <mistergddora2000@gmail.com>
2023-09-14 19:09:31 +0200

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -5,16 +5,16 @@
 *Return:returns nothing
 */
 void
-print_line (int n)
+print_line(int n)
 {
-  if (n <= 0)
-    {
-      putchar ("\n");
-      return;
-    }
-  for (int i = 0; i < n; i++)
-    {
-      putchar ("_");
-    }
-  putchar ("\n");
+if (n <= 0)
+{
+putchar ("\n");
+return;
+}
+for (int i = 0; i < n; i++)
+{
+putchar ("_");
+}
+putchar ("\n");
 }
