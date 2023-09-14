2dfa198d1cd4217fbc0ac5ab7747f594debd50c7
mg5714 <mistergddora2000@gmail.com>
2023-09-14 15:42:01 +0200

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*print_numbers- function that print_numbers from 0to 9.
+*Return: 0
+*/
+void print_numbers(void)
+{
+for (i = 0; i < 10; i++)
+{
+putchar('0' + i);
+}
+putchar('\n');
+return (0);
+}
