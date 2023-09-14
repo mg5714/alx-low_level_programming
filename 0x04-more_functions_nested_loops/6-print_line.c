7fdc1f529aa9ce7735cec4e1d303e0edb8862907
mg5714 <mistergddora2000@gmail.com>
2023-09-14 19:04:51 +0200

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*print_line - prints a straight line
+*@n:parameter
+*Return:returns nothing
+*/
+void
+print_line (int n)
+{
+  if (n <= 0)
+    {
+      _putchar ("\n");
+      return;
+    }
+  for (int i = 0; i < n; i++)
+    {
+      _putchar ("_");
+    }
+  _putchar ("\n");
+}
