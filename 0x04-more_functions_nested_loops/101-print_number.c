53c2af97d581e142315985ccd8c2b3414aaa46e6
mg5714 <mistergddora2000@gmail.com>
2023-09-15 04:09:45 +0200

    task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,15 +5,19 @@
 */
 void print_number(int n)
 {
-unsigned int num = n;
+unsigned int num;
 if (n < 0)
 {
-putchar('-');
-num = -num;
+num = -n;
+_putchar('-');
 }
-if ((num / 10) > 0)
+else
+{
+num = n;
+}
+if (num / 10)
 {
 print_number(num / 10);
 }
-putchar((num % 10) + '0');
+_putchar((num % 10) + '0');
 }
