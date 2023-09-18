9cd161f97b066e0729eca0746b5c2bb48bbc87cd
mg5714 <mistergddora2000@gmail.com>
2023-09-18 17:17:57 +0200

    task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,7 +8,10 @@ void rev_string(char *s)
 {
 int i;
 int len = *s.length();
- int n = len;
- for (i = (n - 1); i >= 0; i--)
+int n = len;
+for (i = (n - 1); i >= 0; i--)
+{
 putchar(s[i]);
+}
+putchar('\n');
 }
