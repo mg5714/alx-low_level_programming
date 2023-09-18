d6ca551e280d7eca44dd51a6f3ed0b27b88858c3
mg5714 <mistergddora2000@gmail.com>
2023-09-18 15:28:12 +0200

    holaa

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -25,15 +25,4 @@ char *_strcpy(char *dest, char *src);
 
 int _atoi(char *s);
 
-#include <unistd.h>
-/**
-* _putchar - writes the character c to stdout
-* @c: The char to print
-* Return: on success 1, on error -1.
-*/
-int _putchar(char c)
-{
-    return (write(1, &c, 1));
-}
-
 #endif /* MAIN_H */
