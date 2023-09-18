6f2be1073c78ff7c9df277b1944eef25e07e42b0
mg5714 <mistergddora2000@gmail.com>
2023-09-18 21:59:05 +0200

    task 11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - program that generates random valid
+* passwords for the program 101-crackme
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int x;
+char c;
+srand(time(NULL));
+while (x <= 2645)
+{
+c = rand() % 128;
+x += c;
+putchar(c);
+}
+putchar(2772 - sum);
+return (0);
+}
