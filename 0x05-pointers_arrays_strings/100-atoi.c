53ddca720011faf31200b253ef88287b2f25062d
mg5714 <mistergddora2000@gmail.com>
2023-09-18 21:07:15 +0200

    task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -13,10 +13,10 @@ int sign = 1;
 do {
 if (*s == '-')
 sign *= -1;
-else if (*s >= '0' && *s <= '9')
-  result = (result * 10) + (*s - '0');
+else if (*s >= '0', && *s <= '9')
+result = (result * 10) + (*s - '0');
 else if (result > 0)
 break;
 } while (*s++);
-return (result * sign );
+return (result * sign);
 }
