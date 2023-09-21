2eaf78f040511e5e247309bf91959cebf1b28fce
mg5714 <mistergddora2000@gmail.com>
2023-09-21 20:43:23 +0200

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -22,3 +22,22 @@ i++;
 }
 return (str);
 }
+#include "main.h"
+/**
+ * cap_string - capitalizes most of the words in a string.
+ * @str: analized string.
+ *
+ * Return: Str.
+ */
+char *cap_string(char *str)
+{
+char *token;
+char separators[] = " \t\n,;.!?\"(){}";
+token = strtok(str, separators);
+while (token != NULL)
+{
+token[0] = toupper(token[0]);
+token = strtok(NULL, separators);
+}
+return (str);
+}
