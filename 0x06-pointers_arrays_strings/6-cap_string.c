60cb16f8649e65448b2f5272222159479c63ef7e
mg5714 <mistergddora2000@gmail.com>
2023-09-21 21:02:41 +0200

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,19 +8,27 @@
 char *cap_string(char *str)
 {
 int i = 0;
-while(str[i] != '\0')
+while (str[i] != '\0')
 {
-if(i == 0 || str[i-1] == ' ' || str[i-1] == '\t' || str[i-1] == '\n' || str[i-1] == ',' || str[i-1] == ';' || str[i-1] == '.' ||
- str[i-1] == '!' || str[i-1] == '?' || str[i-1] == '"' || str[i-1] == '(' || str[i-1] == ')' || str[i-1] == '{' || str[i-1] == '}')
+if(i == 0 ||
+ str[i-1] == ' ' ||
+ str[i-1] == '\t' ||
+ str[i-1] == '\n' ||
+ str[i-1] == ',' ||
+ str[i-1] == ';' ||
+ str[i-1] == '.' ||
+ str[i-1] == '!' ||
+ str[i-1] == '?' ||
+ str[i-1] == '"' ||
+ str[i-1] == '(' ||
+ str[i-1] == ')' ||
+ str[i-1] == '{' ||
+ str[i-1] == '}')
 {
 if(str[i] >= 'a' && str[i] <= 'z')
 {
-str[i] = str[i] - ('a' - 'A');
-}
-else
-{
-
-}
+str[i] = str[i] - 32;
+} 
 }
 i++;
 }
