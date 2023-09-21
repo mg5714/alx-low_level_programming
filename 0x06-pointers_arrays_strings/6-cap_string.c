6e27115e79fa0970f18b433beab93ed9d4575613
mg5714 <mistergddora2000@gmail.com>
2023-09-21 21:18:45 +0200

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,27 +7,30 @@
  */
 char *cap_string(char *str)
 {
-int i = 0;
-while (str[i])
-{
-while (!(str[i] >= 'a' && str[i] <= 'z'))
-i++;
-if(str[i-1] == ' ' ||
-str[i-1] == '\t' ||
-str[i-1] == '\n' ||
-str[i-1] == ',' ||
-str[i-1] == ';' ||
-str[i-1] == '.' ||
-str[i-1] == '!' ||
-str[i-1] == '?' ||
-str[i-1] == '"' ||
-str[i-1] == '(' ||
-str[i-1] == ')' ||
-str[i-1] == '{' ||
-str[i-1] == '}' ||
-i == 0)
-str[i] = str[i] - 32;
-i++;
-}
-return (str);
+	int i, j;
+	int a[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+
+	i = 0;
+	while (*(str + i) != '\0')
+	{
+		if (*(str + i) >= 'a' && *(str + i) <= 'z')
+		{
+			if (i == 0)
+			{
+				*(str + i) = *(str + i) - 32;
+			}
+			else
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == *(str + i - 1))
+					{
+						*(str + i) = *(str + i) - 32;
+					}
+				}
+			}
+		}
+	i++;
+	}
+	return (str);
 }
