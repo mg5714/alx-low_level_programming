8f2bdad73f3289af9db6351f3d236c6864a670a9
mg5714 <mistergddora2000@gmail.com>
2023-09-21 17:03:55 +0200

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,9 +9,7 @@
 char *_strncat(char *dest, char *src, int n)
 {
 int i, j;
-i = 0;
-while (dest[i])
-i++;
+for (i = 0; dest[i] != '\0'; i++);
 for (j = 0; src[j] != '\0' &&  j < n; j++)
 dest[i + j] = src[j];
 dest[i + j] = '\0';
