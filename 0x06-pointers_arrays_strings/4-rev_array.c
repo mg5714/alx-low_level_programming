ea20f6591dbe625896633992971c6ef4fdbdb683
mg5714 <mistergddora2000@gmail.com>
2023-09-21 18:44:46 +0200

    task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -5,13 +5,14 @@
 * @n: number of elements of the array
 * Return: reversed array.
 */
-void reverse_array(int *a, int n)
+void
+reverse_array (int *a, int n)
 {
-int i, temp;
-for (i = 0; i < n; i++)
-{
-temp = a[i];
-a[i] = a[n - 1 - i];
-a[n - 1 - i] = temp;
-}
+  int i, temp;
+  for (i = 0; i < n; i++)
+    {
+      temp = a[i];
+      a[i] = a[n - 1 - i];
+      a[n - 1 - i] = temp;
+    }
 }
