bffc9022bfd67f55ae7732dbda13f1c6e8c15e50
mg5714 <mistergddora2000@gmail.com>
2023-09-21 15:34:19 +0200

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,19 +5,19 @@
  * @src: string to be concatenated
  * Return: dest.
  */
-char *_strcat (char *dest, char *src)
+char *_strcat(char *dest, char *src)
 {
 size_t i = 0, j = 0;
-while (dest[i] != '\0')
+while (*dest[i] != '\0')
 {
 i++;
 }
-while (src[j] != '\0')
+while (*src[j] != '\0')
 {
-dest[i] = src[j];
+*dest[i] = *src[j];
 i++;
 j++;
 }
-dest[i] = '\0';
-return dest;
+*dest[i] = '\0';
+return (dest);
 }
