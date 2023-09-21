300f6ac9908a56cfb16cf7e16c640379372bc21f
mg5714 <mistergddora2000@gmail.com>
2023-09-21 19:29:26 +0200

    task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,13 +7,14 @@
 */
 char *string_toupper(char *str)
 {
-int i;
-for (i = 0; str[i] != '\0'; i++)
+int i = 0;
+while (*(str + i) != '\0')
 {
-if(str[i] >= 97 && str[i] <= 122)
+if (*(str + i) >= 'a' && *(str + i) <= 'z')
 {
-str[i] = str[i] - 32;
+*(str + i) = *(str + i) - 32;
 }
+i++;
 }
 return (str);
 }
