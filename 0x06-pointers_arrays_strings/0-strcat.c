2b8cd8e3ff0ccff44a902a359d85db88fea9132d
mg5714 <mistergddora2000@gmail.com>
2023-09-21 15:35:13 +0200

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,16 +8,16 @@
 char *_strcat(char *dest, char *src)
 {
 size_t i = 0, j = 0;
-while (*dest[i] != '\0')
+while (dest[i] != '\0')
 {
 i++;
 }
-while (*src[j] != '\0')
+while (src[j] != '\0')
 {
-*dest[i] = *src[j];
+dest[i] = src[j];
 i++;
 j++;
 }
-*dest[i] = '\0';
+dest[i] = '\0';
 return (dest);
 }
