2811a3977f03584fe4b98488b6a1531b4d746737
mg5714 <mistergddora2000@gmail.com>
2023-09-21 20:44:24 +0200

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,30 +7,6 @@
  */
 char *cap_string(char *str)
 {
-int i = 0;
-while(str[i] != '\0')
-{
-if(i == 0 || str[i-1] == ' ' || str[i-1] == '\t' || str[i-1] == '\n' || str[i-1] == ',' || str[i-1] == ';' || str[i-1] == '.' ||
-str[i-1] == '!' || str[i-1] == '?' || str[i-1] == '"' || str[i-1] == '(' || str[i-1] == ')' || str[i-1] == '{' || str[i-1] == '}')
-{
-if(str[i] >= 'a' && str[i] <= 'z')
-{
-str[i] = str[i] - ('a' - 'A');
-}
-}
-i++;
-}
-return (str);
-}
-#include "main.h"
-/**
- * cap_string - capitalizes most of the words in a string.
- * @str: analized string.
- *
- * Return: Str.
- */
-char *cap_string(char *str)
-{
 char *token;
 char separators[] = " \t\n,;.!?\"(){}";
 token = strtok(str, separators);
