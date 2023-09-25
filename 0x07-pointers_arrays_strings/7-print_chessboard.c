5fd6839e19f8bda6ee7c2f1917d269284275a07c
mg5714 <mistergddora2000@gmail.com>
2023-09-25 21:59:20 +0200

    task 6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,7 +2,7 @@
 /**
  * print_chessboard - a function that prints the chessboard.
  *@a: to printed
-*
+* Return: none
 */
 void print_chessboard(char (*a)[8]);
 {
@@ -11,7 +11,6 @@ for (int i = 0; i < 8; i++)
 for (int j = 0; j < 8; j++)
 {
 putchar(a[i][j]);
-putchar(' ');
 }
 putchar('\n');
 }
