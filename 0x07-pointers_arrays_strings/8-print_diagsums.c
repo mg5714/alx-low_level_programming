248ef1a9b8d795924feb9214397d035564d2fa15
mg5714 <mistergddora2000@gmail.com>
2023-09-25 23:08:15 +0200

    task 7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -17,7 +17,7 @@ for (i = 0; i < size; i++)
 sum1 = sum1 + a[i *size + i];
 for (i = size - 1; i >= 0 ; i--)
 {
-sum2 += a[y *size + (size - i - 1)];
+sum2 += a[i *size + (size - i - 1)];
 }
 }
 printf("%d, %d\n", sum1, sum2);
