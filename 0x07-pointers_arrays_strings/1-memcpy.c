8ab695c7cb87fe7339e74fbe3f744e176b0cda6b
mg5714 <mistergddora2000@gmail.com>
2023-09-25 15:31:40 +0200

    task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,12 +8,11 @@
  * Return: a pointer to dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
-{ 
-	unsigned int i;
-	
-	for (int i = 0; i < n; i++)
-	{
-		dest[i] = src[i];	
-	} 
-    return (dest);
+{
+unsigned int i;
+for (int i = 0; i < n; i++)
+{
+dest[i] = src[i];
+}
+return (dest);
 } 
