619d5b58d76a072be6de13e20f451de11decf248
mg5714 <mistergddora2000@gmail.com>
2023-09-25 22:33:50 +0200

    task 7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -6,15 +6,15 @@
 *@a: diagonals arrays of int
 *@size: size of arry
 */
-void print_diagsums(int *a, int size)
+void print_diagsums (int *a, int size)
 {
-int i, sum1 = 0, sum2 = 0;
-for (i = 0; i < size; i++)
-{
-sum1 += a[i];
-sum2 += a[size - i - 1];
-a += size;
-}
-printf("Sum of diagonal 1: %d\n", sum1);
-printf("Sum of diagonal 2: %d\n", sum2);
+  int i, sum1 = 0, sum2 = 0;
+  for (i = 0; i < size; i++)
+    {
+      sum1 += a[i];
+      sum2 += a[size - i - 1];
+      a += size;
+    }
+  printf ("Sum of diagonal 1: %d\n", sum1);
+  printf ("Sum of diagonal 2: %d\n", sum2);
 }
