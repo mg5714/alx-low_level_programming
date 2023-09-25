913e3321262c8773cb52fb8ab1c29812a3d2fb08
mg5714 <mistergddora2000@gmail.com>
2023-09-25 15:52:04 +0200

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * _strchr - function that locates a character in a string
+ *@s: specifies the pointer to be performed.
+*@c: specifies the character to be searched.
+*
+* Return: char with result
+*/
+char *_strchr(char *s, char c)
+{
+while (*s != '\0')
+{
+if (*s == c)
+{
+return (s);
+}
+s++;
+}
+return (NULL);
+}
