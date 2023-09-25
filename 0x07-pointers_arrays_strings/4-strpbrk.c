fec1895aef28b1b008eff6dd44dc04ae9d248401
mg5714 <mistergddora2000@gmail.com>
2023-09-25 23:16:58 +0200

    task 7

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -8,14 +8,15 @@
 */
 char *_strpbrk(char *s, char *accept)
 {
-int i, j;
-for (i = 0; s[i] != '\0'; i++)
+int j;
+while (*s)
 {
 for (j = 0; accept[j] != '\0'; j++)
 {	
 if (accept[j] == s[i])
 return (s);
 }
+s++;
 }
-return (0);
+return ('\0');
 }
