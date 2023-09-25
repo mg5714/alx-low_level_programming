67154dea50c514e82e622e526d32b4afc52865b7
mg5714 <mistergddora2000@gmail.com>
2023-09-25 21:31:55 +0200

    task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,24 +7,19 @@
 * Return: 0 alwayes
 */
 char *_strstr(char *haystack, char *needle)
-{    if (!*needle)
 {
-return (haystack);
-}
-char *p1 = haystack;
-while (*p1)
+char *p, *p2;
+for (; *haystack != '\0'; haystack++)
 {
-char *p1Begin = p1, *p2 = needle;
-while (*p1 && *p2 && *p1 == *p2)
+*p = haystack;
+*p2 = needle;
+while (*p == *p2 && *p2 != '\0')
 {
-p1++;
+p++;
 p2++;
 }
-if (!*p2)
-{
-return (p1Begin);
-}
-p1 = p1Begin + 1;
+if (*p2 == '\0')
+return (haystack);
 }
 return (NULL);
 }
