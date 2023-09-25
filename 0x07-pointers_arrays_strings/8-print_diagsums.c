ddfb8d44b5d187f537eab91785b98acc73b9f5ee
mg5714 <mistergddora2000@gmail.com>
2023-09-26 00:55:06 +0200

    task 9

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -15,6 +15,6 @@ sum1 += a[i];
 sum2 += a[size - i - 1];
 a += size;
 }
-printf("Sum of diagonal 1: %d\n", sum1);
-printf("Sum of diagonal 2: %d\n", sum2);
+printf("%d, ", sum1);
+printf("%d\n", sum2);
 }
