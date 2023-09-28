0948477d092dd459a827b232dbdbb8feb3dfd768
mg5714 <mistergddora2000@gmail.com>
2023-09-28 19:14:22 +0200

    task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -19,13 +19,13 @@ return (is_prime(n, 1));
 */
 int is_prime(int n, int x)
 {
-if (n <= 1 || n % x == 0)
-{
-return (0);
-}
-else if (n > 1 && x >= n)
+if (n >= 2 && x >= n)
 {
 return (1);
+}
+else if (n % x == 0 ||  n <= 1)
+{
+return (0);
 }
 else
 return (is_prime(n, x + 1));
