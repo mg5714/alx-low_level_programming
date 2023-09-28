c12256486d5d4c77ab69a1c672e9c43206785577
mg5714 <mistergddora2000@gmail.com>
2023-09-28 18:08:57 +0200

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -19,7 +19,7 @@ if (n == 0 || n == 1)
 return (n);
 }
 else
-return (_sqrt_helper(n, 1, n));    
+return (_sqrt_helper(n, 1, n));
 }
 /**
  * _sqrt_helper - a function that returns the natural square
@@ -34,8 +34,8 @@ int _sqrt_helper(int n, int start, int end)
 int mid = (start + end) / 2;
 if (mid * mid == n)
 return (mid);
-else if (mid * mid > n)
-return (_sqrt_helper(n, start, mid - 1));
+else if (mid * mid < n)
+return (_sqrt_helper(n, start, mid + 1));
 else
 return (_sqrt_helper(n, mid + 1, end));
 }
