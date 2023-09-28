44acf10e96fe831274a2fd1b4a44dbe3167d152d
mg5714 <mistergddora2000@gmail.com>
2023-09-28 18:10:47 +0200

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -35,7 +35,7 @@ int mid = (start + end) / 2;
 if (mid * mid == n)
 return (mid);
 else if (mid * mid > n)
-return (_sqrt_helper(n, start, mid + 1));
+return (_sqrt_helper(n, start, mid - 1));
 else
 return (_sqrt_helper(n, mid + 1, end));
 }
