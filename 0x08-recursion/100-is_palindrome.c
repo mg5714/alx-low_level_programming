4b41d881fec321fbb36fc8269dc374fb754230c4
mg5714 <mistergddora2000@gmail.com>
2023-09-28 21:02:21 +0200

    task 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -8,14 +8,14 @@
 *@e: num
 * Return: 1 if a string is a palindrome and 0 if not.
 */
-int isPalRec(char s[], int x, int e)
+int is_palin(char s[], int x, int e)
 {
 if (x == e)
 return (1);
 if (s[x] != s[e])
 return (0);
 if (x < e + 1)
-return (isPalRec(s, x + 1, e - 1));
+return (is_palin(s, x + 1, e - 1));
 return (1);
 }
 int is_palindrome(char *s)
@@ -23,5 +23,5 @@ int is_palindrome(char *s)
 int n = strlen(s);
 if (n == 0)
 return (1);
-return (isPalRec(s, 0, n - 1));
+return (is_palin(s, 0, n - 1));
 }
