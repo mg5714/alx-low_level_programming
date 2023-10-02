cea890ef2274bd817203e53af76f6f09891e3d11
mg5714 <mistergddora2000@gmail.com>
2023-10-02 18:09:08 +0200

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints the number of arguments passed into it
+ * @argc:  arguments cuonter
+ * @argv: arguments value
+ * Return: 0
+ */
+int main(int argc, char *argv[])   
+{
+    int i;
+    for (i = 0; i < argc; i++)
+    {
+        printf("%s ", argv[i]);       
+    }
+    printf("\n");
+}
