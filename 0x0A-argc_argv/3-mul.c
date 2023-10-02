a0a8e515490108072de4ba22659d7b3b72be6277
mg5714 <mistergddora2000@gmail.com>
2023-10-02 19:06:08 +0200

    task 2

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,14 +9,18 @@
  */
 int main(int argc, char *argv[])
 {
-  int num1, num2;
+int num1, num2;
 if (argc == 1 || argc == 2)
 {
 printf("Error\n");
 return (1);
 }
+else
+{
+num2 = 1;
 for (num1 = 1; num1 < 3; num1++)
 num2 *= atoi(argv[num1]);
 printf("%d\n", num2);
+}
 return (0);
 }
