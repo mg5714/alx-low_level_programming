31e648e2887dc0cb8860ccd25300ad93f1136189
mg5714 <mistergddora2000@gmail.com>
2023-10-02 18:12:57 +0200

    task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,10 +8,5 @@
  */
 int main(int argc, char *argv[])   
 {
-    int i;
-    for (i = 0; i < argc; i++)
-    {
-        printf("%s ", argv[i]);       
-    }
-    printf("\n");
+    printf("%d\n", argc);
 }
