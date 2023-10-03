167666192e26ad6074883e3d9cb761be63b5cdff
mg5714 <mistergddora2000@gmail.com>
2023-10-03 02:40:11 +0200

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -5,11 +5,11 @@
  * main - a program that adds positive numbers.
  * @argc:  arguments cuonter
  * @argv: arguments value
- * Return: 1 if a noninteger , 0 otherwise
+ * Return: 0, If does not receive two arguments return 1.
  */
 int main(int argc, char *argv[])
 {
-int i, j, len, sum;
+int i, sum;
 char num;
 if (argc == 1)
 {
@@ -22,13 +22,10 @@ sum = 0;
 for (i = 1; i < argc; i++)
 {
 num = atoi(argv[i]);
-for (j = 0; j < len; j++)
-{
 if (num == 0 && argv[i][0] != '0')
 {
 printf("Error\n");
 return (1);
-}
 }
 if (num > 0)
 {
