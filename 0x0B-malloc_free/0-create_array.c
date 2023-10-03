85d060e032c94ca653110dfbca3672f867aaa131
mg5714 <mistergddora2000@gmail.com>
2023-10-03 15:44:28 +0200

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,14 +8,13 @@
  */
 char *create_array(unsigned int size, char c)
 {
-    int i;
-    char *arr;
+     unsigned int i;
     if (size == 0)
     {
         return (NULL);
     }
     
-    *arr = malloc(size * sizeof(char));
+    char *arr = malloc(size * sizeof(char));
     
     if (arr == NULL)
     {
