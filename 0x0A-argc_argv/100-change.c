749603c48ce290d7647d40989d8f6b75fabfd1d7
mg5714 <mistergddora2000@gmail.com>
2023-10-03 03:29:49 +0200

    task 5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -8,31 +8,31 @@
  */
 int main(int argc, char *argv[])
 {
-    if (argc == 2)
-    {
-        int i, lastcent = 0, mon = atoi(argv[i]);
-        int cents[] = {25, 10, 5, 2, 1};
+if (argc == 2)
+{
+int i, lastcent = 0, mon = atoi(argv[i]);
+int cents[] = {25, 10, 5, 2, 1};
 
-        for (i = 0; i < 5; i++)
-        {
-            if (mon >= cents[i])
-            {
-                lastcent += mon / cents[i];
-                mon = mon % cents[i];
-                if (mon % cents[i] == 0)
-                {
-                    break;
-                }
-                
-            }
-            
-        }
-        printf("%d\n", lastcent);
-    }
-    else
-    {
-        printf("Error\n");
-        return (1);
-    }
-    return (0);
+for (i = 0; i < 5; i++)
+{
+if (mon >= cents[i])
+{
+lastcent += mon / cents[i];
+mon = mon % cents[i];
+if (mon % cents[i] == 0)
+{
+break;
+}
+
+}
+
+}
+printf("%d\n", lastcent);
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+return (0);
 }
