d90e88e5dccc3a5c942e9b6042854f5a17e7c21f
mg5714 <mistergddora2000@gmail.com>
2023-10-03 02:37:20 +0200

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,33 +9,32 @@
  */
 int main(int argc, char *argv[])
 {
-int i, j, len, sum;
-char num;
-if (argc == 1)
-{
-printf("0\n");
-return (0);
-}
-else
-{
-sum = 0;
-for (i = 1; i < argc; i++)
-{
-num = atoi(argv[i]);
-for (j = 0; j < len; j++)
-{
-if (isdigit(num == 0)
-{
-printf("Error\n");
-return (1);
-}
-}
-if (num > 0)
-{
-sum += num;
-}
-}
-printf("%d\n", sum);
-}
-return (0);
+	int i, j, length, sum;
+	char *ptr;
+
+	if (argc < 2)
+		printf("0\n");
+	else
+	{
+		sum = 0;
+		for (i = 1; i < argc; i++)
+		{
+			ptr = argv[i];
+			length = strlen(ptr);
+
+			for (j = 0; j < length; j++)
+			{
+				if (isdigit(*(ptr + j)) == 0)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+
+			sum += atoi(argv[i]);
+		}
+
+	printf("%d\n", sum);
+	}
+	return (0);
 }
