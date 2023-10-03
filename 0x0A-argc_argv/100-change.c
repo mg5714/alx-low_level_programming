f08696c13b34253cd4d7e6b3a1b9a9ec34e5b545
mg5714 <mistergddora2000@gmail.com>
2023-10-03 03:13:16 +0200

    task 5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -6,46 +6,53 @@
  * @argv: arguments value
  * Return: 0 success return if not 1.
  */
+
 int main(int argc, char *argv[])
 {
-int cents, num_coins, remaining;
-if (argc != 2)
-{
-printf("Error\n");
-return (1);
-}
-int cents = atoi(argv[1]);
-if (cents < 0)
-{
-printf("0\n");
-return (0);
-}
-num_coins = 0;
-remaining = cents;
-while (remaining > 0)
-{
-if (remaining >= 25)
-{
-num_coins++;
-remaining -= 25;
-} else if (remaining >= 10)
-{
-num_coins++;
-remaining -= 10;
-} else if (remaining >= 5)
-{
-num_coins++;
-remaining -= 5;
-} else if (remaining >= 2)
-{
-num_coins++;
-remaining -= 2;
-} else
-{
-num_coins++;
-remaining -= 1;
-}
-}
-printf("%d\n", num_coins);
-return (0);
+    int cents, num_coins, remaining;
+    if (argc != 2)
+    {
+        printf("Error\n");
+        return (1);
+    }
+    
+    int cents = atoi(argv[1]);
+    
+    if (cents < 0)
+    {
+        printf("0\n");
+        return (0);
+    }
+    
+    num_coins = 0;
+    remaining = cents;
+    
+    while (remaining > 0)
+    {
+        if (remaining >= 25)
+        {
+            num_coins++;
+            remaining -= 25;
+        } else if (remaining >= 10)
+        {
+            num_coins++;
+            remaining -= 10;
+        } else if (remaining >= 5)
+        {
+            num_coins++;
+            remaining -= 5;
+        } else if (remaining >= 2)
+        {
+            num_coins++;
+            remaining -= 2;
+        } else
+        {
+            num_coins++;
+            remaining -= 1;
+        }
+    }
+    
+    printf("%d\n", num_coins);
+    
+    return (0);
 }
