78e4bbd104b63556a679441e277a9f1328c87b37
mg5714 <mistergddora2000@gmail.com>
2023-10-04 19:26:39 +0200

    task 5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -10,14 +10,13 @@
 */
 char *argstostr(int ac, char **av)
 {
-int total_len, i, index = 0;
+int total_len = 0, i, index = 0;
 char *result;
 if (ac == 0 || av == NULL)
 {
 return (NULL);
 }
-total_len = 0;
-for (i = 0; i < ac; i++)
+for (i = 0; i < ac; i++, total_len++)
 {
 total_len += strlen(av[i]);
 }
