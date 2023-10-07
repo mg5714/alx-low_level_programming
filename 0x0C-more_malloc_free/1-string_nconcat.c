06f181c3a9b5f55a71d1a53d5698eeec22d95a39
mg5714 <mistergddora2000@gmail.com>
2023-10-08 01:20:39 +0200

    task 0

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -21,15 +21,13 @@ while (s1[len1] != '\0')
 len1++;    
 while (s2[len2] != '\0')
 len2++;    
-if (n >= len2)
-n = len2;    
 result = malloc((len1 + n + 1) * sizeof(char));
 if (result == NULL)
 return (NULL);
 for (i = 0; i < len1; i++)
 result[i] = s1[i];    
 for (i = 0; i < n; i++)
-result[len1 + i] = s2[i];
-result[len1 + n] = '\0';
+result[i] = s2[i];
+result[len1] = '\0';
 return (result);
 }
