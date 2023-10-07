9321dce782e5f28f7fcb7e8962cb6c1075e29522
mg5714 <mistergddora2000@gmail.com>
2023-10-07 15:50:42 +0200

    task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,1 +1,14 @@
-knjl
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - that allocates memory using malloc.
+ * @b: string to count
+ * Return: pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+    void *ptr = malloc(b);
+    if (ptr == 0)
+        exit(98);
+    return (ptr);
+}
