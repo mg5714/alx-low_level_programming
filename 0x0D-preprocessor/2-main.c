e8644a2a6981f1b91eaf6576c9883042ef8ed944
mg5714 <mistergddora2000@gmail.com>
2023-10-10 22:50:16 +0200

    a

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
 /**
  * main - prints the name of the sursses file
  * 
  * Return: Always 0
  */
-
 int main(void)
 {
-	printf("%s\n", __FILE__);
-	return (1);
+printf("%s\n", __FILE__);
+return (0);
 }
