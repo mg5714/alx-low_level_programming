3ea8a336d509c0a0e88d80f8daa7b853b3c1f518
mg5714 <mistergddora2000@gmail.com>
2023-10-10 18:05:33 +0200

    task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
 
-/**
- * main - prints the name of the file 
- *
- * Return: always 0
- */
-int main(void)
+int main()
 {
-    printf("%s\n", __FILE__);
-    return (0);
+    printf(__FILE__);
+    return 0;
 }
