c7158543216cff549764691368a824a79a61f5c5
mg5714 <mistergddora2000@gmail.com>
2023-10-10 21:47:23 +0200

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -43,32 +43,34 @@ return (dest);
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-char *nameCopy, *ownerCopy;
-dog_t *newDog;
-if (name == NULL || owner == NULL)
-{
-return (NULL);
-}
-newDog = malloc(sizeof(dog_t));
-if (newDog == NULL)
-{
-return (NULL);
-}
-nameCopy = strdup(name);
-if (nameCopy == NULL)
-{
-free(newDog);
-return (NULL);
-}
-ownerCopy = strdup(owner);
-if (ownerCopy == NULL)
-{
-free(nameCopy);
-free(newDog);
-return (NULL);
-}
-newDog->name = nameCopy;
-newDog->age = age;
-newDog->owner = ownerCopy;
-return (newDog);
+    dog_t *newDog;
+    char *nameCopy, *ownerCopy;
+    if (name == NULL || owner == NULL)
+    {
+        return (NULL);
+    }
+    newDog = malloc(sizeof(dog_t));
+    if (newDog == NULL)
+    {
+        return (NULL);
+    }
+    nameCopy = malloc(_strlen(name) + 1);
+    if (nameCopy == NULL)
+    {
+        free(newDog);
+        return (NULL);
+    }
+    ownerCopy = malloc(_strlen(owner) + 1);
+    if (ownerCopy == NULL)
+    {
+        free(nameCopy);
+        free(newDog);
+        return (NULL);
+    }
+    _strcpy(nameCopy, name);
+    _strcpy(ownerCopy, owner);
+    newDog->name = nameCopy;
+    newDog->age = age;
+    newDog->owner = ownerCopy;
+    return (newDog);
 }
