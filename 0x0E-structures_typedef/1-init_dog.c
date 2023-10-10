5aa5d54c08eda92b42fd1f4e6a40b3b70cd075a1
mg5714 <mistergddora2000@gmail.com>
2023-10-10 20:10:22 +0200

    task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -5,12 +5,11 @@
  * @name: name
  * @age: age
  * @owner: owner
- * 
  * Return : void
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-    d->name = name;
-    d->age = age;
-    d->owner = owner;
+d->name = name;
+d->age = age;
+d->owner = owner;
 }
