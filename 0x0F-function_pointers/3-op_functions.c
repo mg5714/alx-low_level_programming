cd36a7a755f13e377ae16f033dd56565941d1bfe
mg5714 <mistergddora2000@gmail.com>
2023-10-11 21:35:47 +0200

    task 3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "calc.h"
+/**
+ *  op_add - add tow int
+ *
+ * @a: first int
+ * @b: sec int
+ * Return : sum
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+/**
+ *  op_sub - sub tow int
+ *
+ * @a: first int
+ * @b: sec int
+ * Return : result
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+/**
+ *  op_mul - mul tow int
+ *
+ * @a: first int
+ * @b: sec int
+ * Return : result
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+/**
+ *  op_div - div tow int
+ *
+ * @a: first int
+ * @b: sec int
+ * Return : result
+ */
+int op_div(int a, int b)
+{
+return (a / b);
+}
+/**
+ *  op_mod - mod tow int
+ *
+ * @a: first int
+ * @b: sec int
+ * Return : result
+ */
+int op_mod(int a, int b)
+{
+return (a % b);
+}
