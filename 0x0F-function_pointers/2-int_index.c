42b0f30af51df6b1d9ff9bf7287f30fc13d7a468
mg5714 <mistergddora2000@gmail.com>
2023-10-11 16:25:46 +0200

    task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+/**
+*array_iterator - a function that searches for an integer.
+* @array: array
+* @size: size of element of arrey
+* @cmp: a pointer used to compare   
+* Return: void
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+if (size <= 0 || array == NULL || cmp == NULL)
+return (-1);
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]) != 0)
+return (i);
+}
+return (-1);
+}
