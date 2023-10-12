3a736c648ceacbfe5b2cabb22218f070198c8f3f
mg5714 <mistergddora2000@gmail.com>
2023-10-12 15:12:57 +0200

    2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -9,8 +9,8 @@
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-unsigned int i ;
 va_list args;
+unsigned int i ;
 char *s;
 va_start(args, n);
 for (i = 0; i < n; i++)
