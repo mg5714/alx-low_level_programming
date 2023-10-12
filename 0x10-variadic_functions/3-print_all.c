25a50982f0b67444712131356a02dc705c66ea94
mg5714 <mistergddora2000@gmail.com>
2023-10-12 16:46:09 +0200

    3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -4,45 +4,45 @@
 /**
  * print_all - prints anything
  * @format: list of types of arguments passed tofunction
- * Return: 0
+ * Return: void
  */
 void print_all(const char * const format, ...)
 {
-	va_list ap;
-	int i = 0, j = 0;
-	char *str;
-    char *c = ", ";
-	va_start(ap, format);
+va_list ap;
+int i = 0, j = 0;
+char *str;
+char *c = ", ";
+va_start(ap, format);
 
-	while (format && format[j])
-		j++;
+while (format && format[j])
+j++;
 
-	while (format && format[i])
-	{
-		if (i  == (j - 1))
-		{
-			c = "";
-		}
-		switch (format[i])
-		{
-		case 'c':
-			printf("%c%s", va_arg(ap, int), c);
-			break;
-		case 'i':
-			printf("%d%s", va_arg(ap, int), c);
-			break;
-		case 'f':
-			printf("%f%s", va_arg(ap, double), c);
-			break;
-		case 's':
-			str = va_arg(ap, char *);
-			if (str == NULL)
-				str = "(nil)";
-			printf("%s%s", str, c);
-			break;
-		}
-		i++;
-	}
-	printf("\n");
-	va_end(ap);
+while (format && format[i])
+{
+if (i  == (j - 1))
+{
+c = "";
+}
+switch (format[i])
+{
+case 'c':
+printf("%c%s", va_arg(ap, int), c);
+break;
+case 'i':
+printf("%d%s", va_arg(ap, int), c);
+break;
+case 'f':
+printf("%f%s", va_arg(ap, double), c);
+break;
+case 's':
+str = va_arg(ap, char *);
+if (str == NULL)
+str = "(nil)";
+printf("%s%s", str, c);
+break;
+}
+i++;
+}
+printf("\n");
+va_end(ap);
 }
