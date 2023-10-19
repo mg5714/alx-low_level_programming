e9fabbfde76a5b4f309d14bfa2178e009b5d6669
mg5714 <mistergddora2000@gmail.com>
2023-10-19 23:21:46 +0200

    task 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,11 @@
 #include "lists.h"
-/* void before_main() __attribute__((constructor)); */
+
 /**
 * before_main - excutes functinbefore the main
 * Return: void.
 */
-void before_main()
+void before_main(void)
 {
-printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,\n"
+"I bore my house upon my back!\n");
 }
