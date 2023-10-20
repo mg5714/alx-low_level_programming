92c0aa763fb5626e1f794866ec957eff9d5bbcc8
mg5714 <mistergddora2000@gmail.com>
2023-10-21 00:40:14 +0200

    task 6

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,1 +1,13 @@
-#include "lists.h"
+section .data
+    msg db 'Hello, Holberton', 0xa, 0
+section .text
+    extern printf
+    global main
+
+main:
+    mov edi, msg
+    mov eax, 0
+    call printf
+
+    xor eax, eax
+    ret
