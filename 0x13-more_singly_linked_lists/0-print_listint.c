1420c53dbdb7dad98811c526d906e6892dbcbbab
mg5714 <mistergddora2000@gmail.com>
2023-10-23 15:44:18 +0200

    task 2 3 4 5

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,7 +5,7 @@
 * @h: pointer to f node
 * Return: num of nodes
 */
-size_t print_listint(const listint_t *h);
+size_t print_listint(const listint_t *h)
 {
 size_t co = 0;
 while (h)
---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+* free_listint2 - a function that frees.
+* @head: pointer to head pointer
+* Return: void.
+*/
+void free_listint2(listint_t **head)
+{
+listint_t *current;
+listint_t *tmp = *head;
+if (head == NULL)
+return;
+while (tmp != NULL)
+{
+current = tmp;
+tmp = tmp->next;
+free(current);
+}
+*head = NULL;
+}
