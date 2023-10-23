c7e72456ede1db5a945040924952a3f8fa28b0a1
mg5714 <mistergddora2000@gmail.com>
2023-10-23 14:26:01 +0200

    task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+* print_listint - a function that prints all the elements
+* @h: pointer to f node
+* Return: num of nodes
+*/
+size_t print_listint(const listint_t *h);
+{
+size_t co = 0;
+while (h)
+{
+printf("%d\n", h->n);
+h = h->next;
+co++;
+}
+return (co);
+}
