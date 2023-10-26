bb1bbd35e3552e827637ccbd2b9233e8513dbc5d
mg5714 <mistergddora2000@gmail.com>
2023-10-26 14:21:09 +0200

    task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * git_bit - that returns the value of a bit at a given index.
- * @b: num of idx
+ * get_bit - that returns the value of a bit at a given index.
+ * @n: num of idx
  * @index: index to get.
  * Return:the value of the bit at index index or -1.
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
 if (index > sizeof(unsigned long int) * 8 - 1)
-return (-1);    
-return (n >> index) & 1;
+return (-1);
+return ((n >> index) & 1);
 }
