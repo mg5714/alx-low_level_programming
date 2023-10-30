b29e3557c76c702487c0fca58f4f41ed53163db7
mg5714 <mistergddora2000@gmail.com>
2023-10-30 19:34:04 +0200

    hello

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,43 +14,36 @@ char buffer[BUFFER_SIZE];
 ssize_t bytes_read;
 if (argc != 3)
 {
-dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-exit(97);
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 }
 file_from = open(argv[1], O_RDONLY);
 if (file_from == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-exit(98);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
 }
 file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 if (file_to == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
-exit(99);
-}    
+dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]), exit(99);
+}
 while ((bytes_read = read(file_from, buffer, BUFFER_SIZE)) > 0)
 {
 if (write(file_to, buffer, bytes_read) != bytes_read)
 {
-dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
-exit(99);
+dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]), exit(99);
 }
 }
 if (bytes_read == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-exit(98);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
 }
 if (close(file_from) == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-exit(100);
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
 }
 if (close(file_to) == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
-exit(100);
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
 }
 return (0);
 }
