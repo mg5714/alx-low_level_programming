9da5cc563150e067f50e829f9e792d335e901290
mg5714 <mistergddora2000@gmail.com>
2023-10-30 17:28:13 +0200

    hello

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,9 +9,11 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 ssize_t fd, rd, wr;
-char *buf = malloc(leaters + 1);
-
+char *buf;
 if (filename == NULL || buf == NULL)
+return (0);
+buf = malloc(letters + 1);
+if (buf == NULL)
 return (0);
 fd = open(filename, O_RDONLY);
 if (fd == -1)
@@ -20,7 +22,20 @@ free(buf);
 return (0);
 }
 rd = read(fd, buf, letters);
+if (rd == -1)
+{
+free(buf);
+close(fd);
+return (0);
+}
+buf[rd] = '\0';
 wr = write(STDOUT_FILENO, buf, rd);
+if (wr == -1 || wr != rd)
+{
+free(buf);
+close(fd);
+return (0);
+}
 free(buf);
 close(fd);
 return (rd);
