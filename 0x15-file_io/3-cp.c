dfdc4843fa03786d1e7ca66247c412ccc231e141
mg5714 <mistergddora2000@gmail.com>
2023-10-30 23:01:56 +0200

    hello

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -24,22 +24,19 @@ file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR
 | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 if (file_to == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]),
-close(file_from), exit(99);
+dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]), exit(99);
 }
 while ((bytes_read = read(file_from, buffer, BUFFER_SIZE)) > 0)
 {
 bytes_written = write(file_to, buffer, bytes_read);
 if (bytes_written != bytes_read)
 {
-dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]),
-close(file_from), close(file_to), exit(99);
+dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]), exit(99);
 }
 }
 if (bytes_read == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
-close(file_from), close(file_to), exit(98);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
 }
 if (close(file_from) == -1)
 {
