d78178f3fe940df377f10ccbfdd5f45de9b321f8
mg5714 <mistergddora2000@gmail.com>
2023-12-07 14:57:46 +0200

    task 0

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -31,7 +31,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (new_node);
 	}
 
-	while (temp && i < idx - 1)
+	while (temp && i != idx - 1)
 	{
 		temp = temp->next;
 		i++;
