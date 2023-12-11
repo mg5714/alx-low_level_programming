d17bb05cfc69954fa98830dadd04f4a6aa53cdbe
mg5714 <mistergddora2000@gmail.com>
2023-12-11 16:10:45 +0200

    task 0

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P /tmp https://github.com/mg5714/alx/raw/main/0x18%20C%20-%20Dynamic%20libraries/libgiga.so
-export LD_PRELOAD=/tmp/libgiga.so
+export LD_PRELOAD=/home/gddora/new_day/alx-low_level_programming/0x18-dynamic_libraries/libgiga.so
