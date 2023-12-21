d5f900a6c8f6b04cb210f9cfc30c42835ceabe4f
mg5714 <mistergddora2000@gmail.com>
2023-12-21 19:57:15 +0200

    task 0

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *node, new_node;
 
-	if (ht == NULL || key == NULL || strlen(key) == 0)
+	if (ht == NULL || key == NULL || *key == '\0')
 	{
 		return (0);
 	}
