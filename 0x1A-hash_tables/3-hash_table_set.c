dc82321c6861a1a1cad1544b37e0d8efc7ea71ef
mg5714 <mistergddora2000@gmail.com>
2023-12-21 20:47:40 +0200

    task 0

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,6 +1,36 @@
 #include "hash_tables.h"
 
 /**
+ * create_node - create new node.
+ * @key: the key.
+ * @size: the node size.
+ * Return: node, or NULL.
+ */
+hash_node_t *create_node(const char *key, const char *value)
+{
+	hash_node_t *new_node;
+
+	new_node = malloc(sizeof(hash_node_t));
+        if (new_node == NULL)
+        {
+                return (NULL);
+        }
+
+        new_node->key = strdup(key);
+        new_node->value = strdup(value);
+
+        if (new_node->key == NULL || new_node->value == NULL)
+        {
+                free(new_node->key);
+                free(new_node->value);
+                free(new_node);
+                return (NULL);
+        }
+	new_node->next = NULL;
+	return (new_node);
+}
+
+/**
  * hash_table_set - function that adds an element to the hash table.
  * @ht : is the hash table you want to add or update the key/value
  * @key : is the key.
@@ -11,7 +41,7 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *node, new_node;
+	hash_node_t *node, *temp;
 
 	if (ht == NULL || key == NULL || *key == '\0')
 	{
@@ -36,24 +66,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		node = node->next;
 	}
 
-	new_node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL)
+	temp = create_node(key, value);
+	if (temp == NULL)
 	{
 		return (0);
 	}
 
-	new_node->key = strdup(key);
-	new_node->value = strdup(value);
-
-	if (new_node->key == NULL || new_node->value == NULL)
-	{
-		free(new_node->key);
-		free(new_node->value);
-		free(new_node);
-	}
-
-	new_node->next = ht->array[index];
-	ht->array[index] = new_node;
+	temp->next = ht->array[index];
+	ht->array[index] = temp;
 
 	return (1);
 }
