2d0dcce7c1bdae1b29151cc768fe4c33cce231ed
mg5714 <mistergddora2000@gmail.com>
2023-12-21 18:51:41 +0200

    task 0

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,5 +1,5 @@
-#ifndef HASH_TABLES_H
-#define HASH_TABLES_H
+#ifndef HASH_TABLES_H_
+#define HASH_TABLES_H_
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -34,8 +34,6 @@ typedef struct hash_table_s
 	hash_node_t **array;
 } hash_table_t;
 
-
-
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
@@ -44,5 +42,4 @@ char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
 
-
-#endif
+#endif /* _HASH_TABLES_H_ */
